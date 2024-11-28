9accdb0ad53512e9ebafbd6bf11c5214607cb19d
Yordy Castillo <u00030588@sise.com.pe.com>
2024-11-27 22:16:54 -0500

    Primer Spring

---
+++ src/main/java/com/example/sesion4/model/User.java
@@ -0,0 +1,32 @@
+package com.example.sesion4.model;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author casti
+ */
+public class User {
+    private String name;
+    private String email;
+    
+    // metodos get and set
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
